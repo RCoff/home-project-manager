23f21df8c54d8182d4f199b8e99a9ad289a9cea1
RCoff <enfaduraa@gmail.com>
2021-12-11 11:51:56 -0500

    Add static root

--- home_project_manager/settings.py
+++ home_project_manager/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 MEDIA_ROOT = BASE_DIR.joinpath("uploads")
 MEDIA_URL = '/uploads/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'interface/static',
