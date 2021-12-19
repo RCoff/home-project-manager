f91236b5cc036423364376669096edf74338bb9b
RCoff <enfaduraa@gmail.com>
2021-12-18 22:50:05 -0500

    Begin methods to dynamically retrieve material information from a url

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import requests
 
+from material import GetMaterialBase
 # Create your views here.
+
+def load_material_from_url():
+    mtl = GetMaterialBase("https://www.homedepot.com/p/Nashua-Tape-1-89-in-x-30-yd-Dryer-Vent-Installation-Duct-Tape-1529836/207203955")
+    print("pause")
+
+
+if __name__ == "__main__":
+    load_material_from_url()
